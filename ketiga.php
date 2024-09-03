e5060e10660921cd34203b6e0812fc04ecbd0cf9
naylamalia <naylanurulamalia@gmail.com>
2024-09-03 17:42:47 +0700

    Praktikum 4C - Membuat Constructor

--- ketiga.php
+++ ketiga.php
@@ -5,6 +5,11 @@ class Lingkaran
     const PHI = 3.14;
     public $jari_jari;
 
+    public function __construct($jari_jari)
+    {
+        $this->jari_jari = $jari_jari;
+    }
+
     public function luas() : float {
         return self::PHI * $this->jari_jari * $this->jari_jari;
     }
@@ -19,6 +24,11 @@ class Bola
     const PHI = 3.14;
     public $jari_jari;
 
+    public function __construct($jari_jari)
+    {
+        $this->jari_jari = $jari_jari;
+    }
+
     public function volume() : float {
         return (4/3) * self::PHI * pow($this->jari_jari, 3);
     }
@@ -29,6 +39,12 @@ class Tabung
     const PHI = 3.14;
     public $jari_jari;
     public $tinggi;
+
+    public function __construct($jari_jari, $tinggi)
+    {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
 
     public function volume() : float {
         return self::PHI * pow($this->jari_jari, 2) * $this->tinggi;
@@ -41,26 +57,26 @@ class Kerucut
     public $jari_jari;
     public $tinggi;
 
+    public function __construct($jari_jari, $tinggi)
+    {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+
     public function volume() : float {
         return (1/3) * self::PHI * pow($this->jari_jari, 2) * $this->tinggi;
     }
 }
 
-$lingkaran = new Lingkaran();
-$lingkaran->jari_jari = 10;
+$lingkaran = new Lingkaran(10);
 echo "Luas Lingkaran: " . $lingkaran->luas() . " cm²\n";
 echo "Keliling Lingkaran: " . $lingkaran->keliling() . " cm\n";
 
-$bola = new Bola();
-$bola->jari_jari = 7;
+$bola = new Bola(7);
 echo "Volume Bola: " . $bola->volume() . " cm³\n";
 
-$tabung = new Tabung();
-$tabung->jari_jari = 5;
-$tabung->tinggi = 15;
+$tabung = new Tabung(5, 15);
 echo "Volume Tabung: " . $tabung->volume() . " cm³\n";
 
-$kerucut = new Kerucut();
-$kerucut->jari_jari = 4;
-$kerucut->tinggi = 10;
-echo "Volume nasi tumpeng adalah: " . $kerucut->volume() . " cm³\n";
+$kerucut = new Kerucut(4, 10);
+echo "Volume Kerucut: " . $kerucut->volume() . " cm³\n";
