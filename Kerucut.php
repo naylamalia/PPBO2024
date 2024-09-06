7de23e01270f0199af82bd174bae260e3b0d484a
naylamalia <naylanurulamalia@gmail.com>
2024-09-06 15:30:22 +0700

    Praktikum 5A - Memisahkan Class

---
+++ Kerucut.php
@@ -0,0 +1,19 @@
+<?php
+class Kerucut
+{
+    const PHI = 3.14;
+    public $jari_jari;
+    public $tinggi;
+
+    public function __construct($jari_jari, $tinggi)
+    {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi = $tinggi;
+    }
+
+    public function volume() : float {
+        return (1/3) * self::PHI * pow($this->jari_jari, 2) * $this->tinggi;
+    }
+}
+$kerucut = new Kerucut(4, 10);
+echo "Volume Kerucut: " . $kerucut->volume() . " cm³\n";
